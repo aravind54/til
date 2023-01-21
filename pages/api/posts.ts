aad34192b9ce4206a27703d3eb6cc08fecffe1a0
aravind54 <aravindcranjan54@gmail.com>
2023-01-21 22:48:34 +0530

    chore: handle slack api commands

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -4,13 +4,17 @@ import Posts from '../../models/postModel'
 const handler = async (req: any, res: any) => {
   try {
     if (req.method === 'POST') {
-      const { content, user } = req.body
+      const { text, user_name } = req.body
       const post = new Posts({
-        content,
-        user,
+        content: text,
+        user: user_name,
       })
       const userCreated = await post.save()
-      res.status(200).send(userCreated)
+      console.log(userCreated)
+      res.status(200).send({
+        response_type: 'in_channel',
+        text: 'TIL is saved successfully!',
+      })
     } else if (req.method === 'GET') {
       const posts = await Posts.find({})
       res.status(200).send({ data: posts })
